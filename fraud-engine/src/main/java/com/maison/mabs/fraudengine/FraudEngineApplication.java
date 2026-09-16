79d7c1a57ab55f7e0dd8dd07fe86ec54550a18bc
Thato Mabeleng <tlmabeleng@gmail.com>
2026-09-16 06:03:11 +0200

    project setup

---
+++ fraud-engine/src/main/java/com/maison/mabs/fraudengine/FraudEngineApplication.java
@@ -0,0 +1,13 @@
+package com.maison.mabs.fraudengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FraudEngineApplication.class, args);
+    }
+
+}
---
+++ fraud-engine/src/test/java/com/maison/mabs/fraudengine/FraudEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maison.mabs.fraudengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
