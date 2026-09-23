7b0db4d126bb3701fda1b9b1d993c4ad0c9f8a02
Thato Mabeleng <tlmabeleng@gmail.com>
2026-09-23 06:17:34 +0200

    fix formatting

--- fraud-engine/src/main/java/com/maison/mabs/fraudengine/FraudEngineApplication.java
+++ fraud-engine/src/main/java/com/maison/mabs/fraudengine/FraudEngineApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FraudEngineApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(FraudEngineApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(FraudEngineApplication.class, args);
+	}
 
 }
